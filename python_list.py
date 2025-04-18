c6217a88eee5b75ba5ee7439e2f1ebe150d0d7f5
hasibudjaman <hasibudjaman70@gmail.com>
2025-04-18 11:19:36 +0600

    third commited

--- python_list.py
+++ python_list.py
@@ -1,2 +1,29 @@
-print("hasib")
-print("github account")
+# print("hasib")
+# print("github account")
+# check if all number are prime
+from itertools import count
+
+num=[3,5,7,13]
+count=0
+for n in num:
+    if n==0 or n==1:
+        print("it is not prime number and False ")
+        break
+    elif n==2:
+        len.append(n)
+
+    if n>2:
+        for i in range(2,n):
+
+            if n%i!=0:
+
+                count=0
+                break
+
+            else:
+                count=1
+
+if count==0:
+    print(True)
+elif count==1:
+    print(False)
